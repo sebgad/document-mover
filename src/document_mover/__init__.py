0137d63bbdb18106ee429375ef12fd6b56897da4
sebgad <->
2025-12-21 17:58:01 +0100

    fix entry points

---
+++ src/document_mover/__init__.py
@@ -0,0 +1,6 @@
+"""Document mover package."""
+
+from document_mover.document_mover import main as document_mover_main
+from document_mover.pdf_merger import main as pdf_merger_main
+
+__all__ = ["document_mover_main", "pdf_merger_main"]
